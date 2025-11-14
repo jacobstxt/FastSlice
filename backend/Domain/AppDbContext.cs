c4ff678834d0a5fce13d8bb79f36aa4124646166
jacobstxt <153942144+jacobstxt@users.noreply.github.com>
2025-11-15 00:02:01 +0200

    Setup project structure: add Domain, Core and initial Category entity

---
+++ backend/Domain/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Domain
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt) { }
+
+        public DbSet<CategoryEntity> Categories { get; set; }
+
+    }
+}
--- backend/FastSlice.Api/Program.cs
+++ backend/FastSlice.Api/Program.cs
@@ -1,3 +1,5 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
 
 namespace FastSlice.Api
 {
@@ -7,22 +9,26 @@ namespace FastSlice.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+              opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
-
+        
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
 
