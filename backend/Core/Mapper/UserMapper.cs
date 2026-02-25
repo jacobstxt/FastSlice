a0933d378996a3af1a45b72b5b59a17b08c45753
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-02-25 21:03:22 +0200

    feat: add user data seeding

---
+++ backend/Core/Mapper/UserMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Core.Models.Category;
+using Core.Models.Seeder;
+using Domain.Entities;
+using Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Mapper
+{
+    public class UserMapper: Profile
+    {
+            public UserMapper()
+            {
+                CreateMap<SeederUserModel, UserEntity>()
+                    .ForMember(opt => opt.UserName, opt => opt.MapFrom(x => x.Email));
+                    
+            }
+        }
+    }
+
--- backend/FastSlice.Api/DbSeeder.cs
+++ backend/FastSlice.Api/DbSeeder.cs
@@ -19,6 +19,7 @@ namespace FastSlice.Api
             var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<RoleEntity>>();
             var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
             var mapper = scope.ServiceProvider.GetRequiredService<IMapper>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<UserEntity>>();
 
             context.Database.Migrate();
 
@@ -68,7 +69,51 @@ namespace FastSlice.Api
             }
 
 
+            if (!context.Users.Any())
+            {
+                var imageService = scope.ServiceProvider.GetRequiredService<IImageService>();
+                var jsonFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "SeedData", "Users.json");
+                if (File.Exists(jsonFile))
+                {
+                    var jsonData = await File.ReadAllTextAsync(jsonFile);
+                    try
+                    {
+                        var users = JsonSerializer.Deserialize<List<SeederUserModel>>(jsonData);
+                        foreach (var user in users)
+                        {
+                            var entity = mapper.Map<UserEntity>(user);
+                            entity.UserName = user.Email;
+                            entity.Image = await imageService.SaveImageFromUrlAsync(user.Image);
+                            var result = await userManager.CreateAsync(entity, user.Password);
+                            if (!result.Succeeded)
+                            {
+                                Console.WriteLine("Error Create User {0}", user.Email);
+                                continue;
+                            }
+                            foreach (var role in user.Roles)
+                            {
+                                if (await roleManager.RoleExistsAsync(role))
+                                {
+                                    await userManager.AddToRoleAsync(entity, role);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Not Found Role {0}", role);
+                                }
+                            }
+                        }
 
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error Json Parse Data {0}", ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Not Found File Users.json");
+                }
+            }
         }
 
 
